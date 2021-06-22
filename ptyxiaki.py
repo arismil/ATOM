e71433247410393bc8aa660be599b80a09b2ab07
Aris Miletis <arismiletis@outlook.com>
2021-06-22 14:11:11 +0300

    First Commit

--- ptyxiaki.py
+++ ptyxiaki.py
@@ -17,12 +17,6 @@ import time
 # opencv stuff
 import cv2
 from preprocessing import lane_detection
-# from lane import Lane
-# print(tf.__version__)
-# device_name = tf.test.gpu_device_name()
-# if device_name != '/device:GPU:0':
-#     raise SystemError('GPU device not found')
-# print('Found GPU at: {}'.format(device_name))
 import detection
 
 
